5b13e282273d23888547f8174bb1384460482fd1
Nan Wang <nwang01@cs.ucsb.edu>
2019-12-02 15:10:30 -0800

    nw create table:JSON to Objects for locations

--- src/main/java/earthquakes/controllers/LocationsController.java
+++ src/main/java/earthquakes/controllers/LocationsController.java
@@ -1,5 +1,8 @@
 package earthquakes.controllers;
 
+ import java.util.List;
+ import earthquakes.osm.Place;
+
  import earthquakes.geojson.FeatureCollection;
  import earthquakes.searches.LocSearch;
  import earthquakes.services.EarthquakeQueryService;
@@ -39,6 +42,8 @@ package earthquakes.controllers;
          model.addAttribute("locSearch", locSearch);
          String json = LQS.getJSON(locSearch.getLocation());
          model.addAttribute("json",json);
+	 List<Place> Places = Place.listFromJson(json);
+         model.addAttribute("Places",Places);
          return "locations/results";
      }
  }
